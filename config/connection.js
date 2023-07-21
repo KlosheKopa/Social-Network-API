5d33205d9a7b6e512e17af682196d62056d7b36e
KlosheKopa <richardsa@my.lanecc.edu>
2023-07-21 15:01:36 -0700

    Finished

---
+++ config/connection.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+const connectionString = 'mongodb://127.0.0.1:27017/social-network-api';
+
+mongoose.connect(connectionString).catch(error => console.error('Error connecting to MongoDB', error));
+
+module.exports = mongoose.connection;
